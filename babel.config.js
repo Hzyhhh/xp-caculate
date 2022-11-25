ecc8dbabdc69e6fe8e065bee3ba8739d82598328
黄梓阳 <huangziyang@ut.cn>
2022-11-25 17:14:02 +0800

    alias config

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,25 @@
 // babel-preset-taro 更多选项和默认值：
 // https://github.com/NervJS/taro/blob/next/packages/babel-preset-taro/README.md
+
+const moduleResolver = [
+  require.resolve("babel-plugin-module-resolver"),
+  {
+    root: ["./"],
+    alias: {
+      "@src": "./src",
+    },
+  },
+];
+
 module.exports = {
   presets: [
-    ['taro', {
-      framework: 'react',
-      ts: true
-    }]
+    [
+      "taro",
+      {
+        framework: "react",
+        ts: true,
+      },
+    ],
   ],
-  plugins: [
-    'react-native-reanimated/plugin',
-  ],
-}
+  plugins: ["react-native-reanimated/plugin", moduleResolver],
+};
